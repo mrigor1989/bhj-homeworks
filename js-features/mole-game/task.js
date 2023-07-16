60906b4a5a2948a0adebcfe05fd23daa3f487475
mfigor1989 <sonin.igor1989@gmail.com>
2023-07-16 14:13:00 +0300

    Все задачи выполнены!

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -10,19 +10,20 @@ function clearCounters(){
     lost.innerText = 0;
 }
 
-for(let i = 1; i < 10; i++) {
+for(let i = 1; i <= 9; i++) {
     let el = getHole(i)
     el.addEventListener('click', ()=> {
         if(el.className.includes('hole_has-mole')) {
             +dead.innerText++;
+            el.classList.remove("hole_has-mole")
             if(+dead.innerText == 10) {
-                alert('win')
+                alert('Вы выиграли!')
                 clearCounters()
             }
         } else {
             +lost.innerText++;
             if(+lost.innerText == 5) {
-                alert('you have lost')
+                alert('Вы проиграли!')
                 clearCounters()
             }
         }
