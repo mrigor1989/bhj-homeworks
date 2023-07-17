73e29d345055636d6488049ca95746be5fe60426
mfigor1989 <sonin.igor1989@gmail.com>
2023-07-17 16:44:02 +0300

    Работа над модальными окнами

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,5 +1,72 @@
-const modal = document.getElementById('modal_main');
-modal.classList.add('modal_active');
-modal.addEventListener('click', () => {
-    modal.classList.add('modal_close')
+const modals = document.querySelectorAll('.modal');
+const modalMain = document.querySelector('#modal_main');
+const modalSuccess = document.querySelector('#modal_success')
+modalMain.classList.add('modal_active');
+
+modals.forEach(el => {
+    el.addEventListener('click', (elem) =>{
+        if(elem.target.classList.contains('modal__close')){
+            el.classList.remove('modal_active')                
+        }
+        if(elem.target.classList.contains('modal_active')){
+            modalSuccess.classList.add('modal_active')
+        } 
+    })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// modals.forEach(modal => {
+//     modal.addEventListener('click', (e)=>{
+//         if(e.target.classList.contains('modal__close')) {
+//             modal.classList.remove('modal_active')
+//         }
+//         if(e.target.classList.contains('show-success')) {
+//             modalSuccess.classList.add('modal_active');
+//         }
+//     })
+// })
