37be2410dad4b6cff03aa774cbe6909ace2e881f
mrigor89 <sonin.igor1989@gmail.com>
2023-08-05 12:04:45 +0300

    В задании с задачами добавил событие на кнопку Eyner

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,7 +3,7 @@ const taskList = document.querySelector('#tasks__list')
 const taskInput = document.querySelector('#task__input')
 const tasks = taskList.querySelectorAll('.task')
 
-btnAdd.addEventListener('click', (e) => {
+addTask = function (e) {
     e.preventDefault()
     if (!taskInput.value == '') {
         const taskEl = document.createElement('div');
@@ -25,10 +25,17 @@ btnAdd.addEventListener('click', (e) => {
         taskEl.appendChild(titleEl);
         taskEl.appendChild(delEl);
 
-        taskList.prepend(taskEl);       
+        taskList.prepend(taskEl);
 
         taskInput.value = '';
     }
 
-})
+}
 
+btnAdd.addEventListener('click', addTask);
+
+document.addEventListener('keydown', (e) => {
+    if (e.keyCode === 13) {
+        addTask;
+    }
+})
